d0a8f093e26f7274ef13b54d3bed2a9f946f2fcf
Romario de M Damasceno <romario.auth@gmail.com>
2024-10-26 20:40:17 -0300

    Criando uma tarefa:
    Uma tarefa é uma unidade de trabalho dentro de um projeto. Cada tarefa possui um título, uma descrição, uma data de vencimento e um status (pendente, em andamento, concluída).

---
+++ src/TaskManagement.Domain/Entities/WorkItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+using TaskManagement.Domain.Common;
+using TaskManagement.Domain.Enum;
+using TaskManagement.Domain.Extensions;
+
+namespace TaskManagement.Domain.Entities
+{
+    public class WorkItem : EntityBase
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime DueDate { get; private set; }
+        public StatusWorkItem StatusWorkItem { get; private set; }
+        public Priority Priority { get; private set; }
+
+        public WorkItem(string title, string description, DateTime dueDate, StatusWorkItem status, Priority priority)
+        {
+            Title = title;
+            Description = description;
+            DueDate = dueDate;
+            StatusWorkItem = status;
+            Priority = priority;
+        }
+
+        public override List<string> Validate()
+        {
+            List<string> validationResult = new List<string>();
+
+            StringExtensions.ValidateField(Description, "Description invalid", validationResult);
+
+            return validationResult;
+        }
+    }
+}
---
+++ src/TaskManagement.Domain/Enum/StatusWorkItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement.Domain.Enum
+{
+    public enum StatusWorkItem
+    {
+        None = 0,
+        Pending = 1,
+        Progress = 2,
+        Completed = 3
+    }
+}
